2dced27ec835823637e151080bc9891360b6229b
ofir511996 <ofir511996@gmail.com>
2019-11-29 10:08:15 +0200

    DataServer class

--- DataServer.cpp
+++ DataServer.cpp
@@ -7,23 +7,23 @@ DataServer::DataServer(DataServerID ds_id, unsigned int num_of_servers):
         ds_id(ds_id), num_of_servers(num_of_servers),
         servers(Array<Server>(num_of_servers)), windows_count(0){
     for(ServerID id=0; id<num_of_servers; ++id){
-        auto server=linux_queue.push_back(id);
-        servers[id]=Server(id, server);
+        auto server_it=linux_queue.push_back(id);
+        servers[id]=Server(id, server_it);
     }
 }
 
 void DataServer::AllocateServer(ServerID id, OS os) {
     Server& server = servers[id];
+    auto& queue = os==Linux ? linux_queue : windows_queue;
     if(server.state==Occupied) {
-        auto& queue = os==Linux ? linux_queue : windows_queue;
         if(queue.empty()) {
             queue = os == Linux ? windows_queue : linux_queue;
             if (queue.empty())
                 throw NoFreeServers();
         }
         server=servers[queue.front()];
-        queue.pop_front();
     }
+    queue.erase(server.iterator);
     server.state=Occupied;
     server.iterator=List<ServerID>::Iterator();
     if(server.os!=os) {
